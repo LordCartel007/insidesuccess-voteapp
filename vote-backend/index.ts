e469c882810bdfecbd16d707b70599c0684e15e8
LordCartel007 <cartellord77@gmail.com>
2025-06-20 14:39:08 +0100

    updated index.js

--- vote-backend/index.ts
+++ vote-backend/index.ts
@@ -69,10 +69,10 @@ app.use("/api/auth", authRoutes);
 
 // console.log("Serving frontend from:", path.join(__dirname, "frontend", "dist"));
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log("Server is running on port:", PORT);
-});
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log("Server is running on port:", PORT);
+// });
 
 // app needs to be exported for vercel to work
 export default app;
